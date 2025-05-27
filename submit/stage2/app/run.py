b6eda788a7ea138fce17498097a9d1e17050fc8c
zhengkai15 <156252108@qq.com>
2025-05-27 09:19:13 +0000

    fix for sais 2025

--- submit/stage2/app/run.py
+++ submit/stage2/app/run.py
@@ -4,12 +4,18 @@ import torch
 import argparse
 
 parser = argparse.ArgumentParser(description="Process EC data.")
-parser.add_argument('--base_name', type=str, required=False, default=f'../tcdata/input')
+parser.add_argument('--base_name', type=str, required=False, default=f'../saisdata/input')
 parser.add_argument('--save_name', type=str, required=False, default=f'./output')
 args = parser.parse_args()
 
 base_name = args.base_name
 save_name = args.save_name
+
+
+channel = ["z50","z100","z150","z200","z250","z300","z400","z500","z600","z700","z850","z925","z1000","t50","t100","t150","t200","t250","t300","t400","t500","t600","t700","t850","t925","t1000","u50","u100","u150","u200","u250","u300","u400","u500","u600","u700","u850","u925","u1000","v50","v100","v150","v200","v250","v300","v400","v500","v600","v700","v850","v925","v1000","q50","q100","q150","q200","q250","q300","q400","q500","q600","q700","q850","q925","q1000","ciwc50","ciwc100","ciwc150","ciwc200","ciwc250","ciwc300","ciwc400","ciwc500","ciwc600","ciwc700","ciwc850","ciwc925","ciwc1000","clwc50","clwc100","clwc150","clwc200","clwc250","clwc300","clwc400","clwc500","clwc600","clwc700","clwc850","clwc925","clwc1000","crwc50","crwc100","crwc150","crwc200","crwc250","crwc300","crwc400","crwc500","crwc600","crwc700","crwc850","crwc925","crwc1000","cswc50","cswc100","cswc150","cswc200","cswc250","cswc300","cswc400","cswc500","cswc600","cswc700","cswc850","cswc925","cswc1000"]
+channel_task = ['t200', 't500', 't700', 't850', 't1000', 'q200','q500', 'q700', 'q850', 'q1000', 'ciwc200','ciwc500', 'ciwc700', 'ciwc850', 'ciwc1000', 'clwc200', 'clwc500', 'clwc700', 'clwc850', 'clwc1000', 'crwc200','crwc500', 'crwc700', 'crwc850', 'crwc1000', 'cswc200','cswc500', 'cswc700', 'cswc850', 'cswc1000']
+c_index = [channel.index(element) for element in channel_task]
+assert len(c_index)==30, "index length should be 30"
 
 def get_sample_input():
     file_names = os.listdir(base_name)
@@ -25,7 +31,7 @@ def inference(file_list):
         input_tensor = torch.load(file_name)
         # output = model(input_tensor)
         print(input_tensor.shape)
-        output = input_tensor[:, :, -1].clone()
+        output = input_tensor[..., c_index, 35:80+1, 70:140+1].clone()
         print(output.shape)
         save_path = file_name.replace(base_name, save_name)
         print(save_path)
--- submit/stage2/app/run.sh
+++ submit/stage2/app/run.sh
@@ -1,14 +1,15 @@
 #!/bin/bash
 echo "program start..."
 # # on my workstation
-# python run.py --base_name "../tcdata/input" --save_name "./output"
+# python run.py --base_name "../saisdata/input" --save_name "./output"
 
 
-python run.py --base_name "/tcdata/input" --save_name "./output"
+python3 run.py --base_name "/saisdata/input" --save_name "./output"
 
-# docker offline debug
-# sudo docker run -it --platform linux/amd64 -v ./tcdata:/tcdata ?? bash 
+# # docker offline debug
+# 将saisdata挂载在根目录, ??为镜像地址
+# sudo docker run -it --platform linux/amd64 -v {saisdata_local}:/saisdata ?? bash 
+# eg:sudo docker run -it --platform linux/amd64 -v ./saisdata:/saisdata ?? bash 
 
-
-
-zip -r -0 output.zip ./output
+save_path=/saisresult
+mkdir -p $save_path && zip -r -0 $save_path/output.zip ./output
